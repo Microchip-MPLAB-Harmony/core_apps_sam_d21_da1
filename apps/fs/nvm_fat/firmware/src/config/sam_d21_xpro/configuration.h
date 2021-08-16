f472d81a00e6eae2fb9583da33f66944efa6bd2d
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-16 19:04:19 +0530

    [SYS_FS] Regenerated demo for fs_nvm_fat_sam_d21_xpro
    MH3-56339
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/fs/nvm_fat/firmware/src/config/sam_d21_xpro/configuration.h
+++ apps/fs/nvm_fat/firmware/src/config/sam_d21_xpro/configuration.h
@@ -56,7 +56,7 @@
 */
 
 #include "user.h"
-#include "toolchain_specifics.h"
+#include "device.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -105,6 +105,7 @@ extern "C" {
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Driver Configuration
@@ -117,6 +118,7 @@ extern "C" {
 #define DRV_MEMORY_BUFFER_QUEUE_SIZE_IDX0    1
 #define DRV_MEMORY_DEVICE_START_ADDRESS      0x20000
 #define DRV_MEMORY_DEVICE_MEDIA_SIZE         128UL
+#define DRV_MEMORY_DEVICE_MEDIA_SIZE_BYTES   (DRV_MEMORY_DEVICE_MEDIA_SIZE * 1024)
 #define DRV_MEMORY_DEVICE_PROGRAM_SIZE       64
 #define DRV_MEMORY_DEVICE_ERASE_SIZE         256
 
