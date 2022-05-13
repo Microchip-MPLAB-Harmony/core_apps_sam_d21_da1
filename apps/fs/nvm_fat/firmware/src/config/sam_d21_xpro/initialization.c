02f2b9a88b9a870fd63b668a81b89daaf1a64a38
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-13 14:23:51 +0530

    [SYS_FS] Regenerated demo for fs_nvm_fat_sam_d21_xpro
    MH3-65256
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/fs/nvm_fat/firmware/src/config/sam_d21_xpro/initialization.c
+++ apps/fs/nvm_fat/firmware/src/config/sam_d21_xpro/initialization.c
@@ -233,7 +233,6 @@ void SYS_Initialize ( void* data )
 	BSP_Initialize();
     NVMCTRL_Initialize( );
 
-    EVSYS_Initialize();
 
 
     sysObj.drvMemory0 = DRV_MEMORY_Initialize((SYS_MODULE_INDEX)DRV_MEMORY_INDEX_0, (SYS_MODULE_INIT *)&drvMemory0InitData);
