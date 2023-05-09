243254462a3333b85737ec4ebd308fd242fec1a7
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-09 15:28:14 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_eeprom_at24_at24_eeprom_read_write_sam_d21_xpro
    MH3-76644

--- apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_d21_xpro/definitions.h
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_d21_xpro/definitions.h
@@ -49,8 +49,8 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "bsp/bsp.h"
-#include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/sercom/i2c_master/plib_sercom2_i2c_master.h"
+#include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "driver/at24/drv_at24.h"
 #include "peripheral/port/plib_port.h"
@@ -71,6 +71,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAMD21J18A"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAMD"
+#define DEVICE_SERIES		 "SAMD21"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 47972352
--- apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_d21_xpro/initialization.c
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_d21_xpro/initialization.c
@@ -56,19 +56,19 @@
 // ****************************************************************************
 #pragma config NVMCTRL_BOOTPROT = SIZE_0BYTES
 #pragma config NVMCTRL_EEPROM_SIZE = SIZE_0BYTES
-#pragma config BOD33USERLEVEL = 0x7 // Enter Hexadecimal value
+#pragma config BOD33USERLEVEL = 0x7U // Enter Hexadecimal value
 #pragma config BOD33_EN = ENABLED
 #pragma config BOD33_ACTION = RESET
 
 #pragma config BOD33_HYST = DISABLED
-#pragma config NVMCTRL_REGION_LOCKS = 0xffff // Enter Hexadecimal value
+#pragma config NVMCTRL_REGION_LOCKS = 0xffffU // Enter Hexadecimal value
 
 #pragma config WDT_ENABLE = DISABLED
 #pragma config WDT_ALWAYSON = DISABLED
 #pragma config WDT_PER = CYC16384
 
 #pragma config WDT_WINDOW_0 = SET
-#pragma config WDT_WINDOW_1 = 0x4 // Enter Hexadecimal value
+#pragma config WDT_WINDOW_1 = 0x4U // Enter Hexadecimal value
 #pragma config WDT_EWOFFSET = CYC16384
 #pragma config WDT_WEN = DISABLED
 
@@ -80,19 +80,23 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
 // <editor-fold defaultstate="collapsed" desc="DRV_AT24 Initialization Data">
 
 /* I2C PLIB Interface Initialization for AT24 Driver */
-const DRV_AT24_PLIB_INTERFACE drvAT24PlibAPI = {
+static const DRV_AT24_PLIB_INTERFACE drvAT24PlibAPI = {
 
     /* I2C PLIB WriteRead function */
     .writeRead = (DRV_AT24_PLIB_WRITE_READ)SERCOM2_I2C_WriteRead,
 
     /* I2C PLIB Write function */
-    .write = (DRV_AT24_PLIB_WRITE)SERCOM2_I2C_Write,
+    .write_t = (DRV_AT24_PLIB_WRITE)SERCOM2_I2C_Write,
 
     /* I2C PLIB Read function */
-    .read = (DRV_AT24_PLIB_READ)SERCOM2_I2C_Read,
+    .read_t = (DRV_AT24_PLIB_READ)SERCOM2_I2C_Read,
 
     /* I2C PLIB Transfer Status function */
     .isBusy = (DRV_AT24_PLIB_IS_BUSY)SERCOM2_I2C_IsBusy,
@@ -105,7 +109,7 @@ const DRV_AT24_PLIB_INTERFACE drvAT24PlibAPI = {
 };
 
 /* AT24 Driver Initialization Data */
-const DRV_AT24_INIT drvAT24InitData =
+static const DRV_AT24_INIT drvAT24InitData =
 {
     /* I2C PLIB API  interface*/
     .i2cPlib = &drvAT24PlibAPI,
@@ -124,8 +128,8 @@ const DRV_AT24_INIT drvAT24InitData =
 
     .blockStartAddress =    0x0,
 };
-
 // </editor-fold>
+
 
 
 // *****************************************************************************
@@ -157,7 +161,7 @@ SYSTEM_OBJECTS sysObj;
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -171,6 +175,8 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
     NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3UL);
 
@@ -183,21 +189,31 @@ void SYS_Initialize ( void* data )
 
 
 	BSP_Initialize();
-    NVMCTRL_Initialize( );
-
     SERCOM2_I2C_Initialize();
 
+    NVMCTRL_Initialize( );
 
+
+
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 
     sysObj.drvAT24 = DRV_AT24_Initialize(DRV_AT24_INDEX, (SYS_MODULE_INIT *)&drvAT24InitData);
 
 
 
 
+    /* MISRAC 2012 deviation block end */
     APP_Initialize();
 
 
     NVIC_Initialize();
+
+
+    /* MISRAC 2012 deviation block end */
 
 }
 
